38641aa7e07f4fcaf1a6d7f871dcc437c3500962
FilipSzkarlat <filip.szkarlat@gmail.com>
2023-12-31 21:12:37 +0100

    active states are ready

---
+++ app.js
@@ -0,0 +1,49 @@
+function Answer1() {
+  if (document.getElementById("answer1").style.display === "none") {
+    document.getElementById("answer1").style.display = "block";
+    document.getElementById("svg1").style.transform = "rotate(180deg)";
+  } else {
+    document.getElementById("answer1").style.display = "none";
+    document.getElementById("svg1").style.transform = "rotate(0deg)";
+  }
+}
+
+function Answer2() {
+  if (document.getElementById("answer2").style.display === "none") {
+    document.getElementById("answer2").style.display = "block";
+    document.getElementById("svg2").style.transform = "rotate(180deg)";
+  } else {
+    document.getElementById("answer2").style.display = "none";
+    document.getElementById("svg2").style.transform = "rotate(0deg)";
+  }
+}
+
+function Answer3() {
+  if (document.getElementById("answer3").style.display === "none") {
+    document.getElementById("answer3").style.display = "block";
+    document.getElementById("svg3").style.transform = "rotate(180deg)";
+  } else {
+    document.getElementById("answer3").style.display = "none";
+    document.getElementById("svg3").style.transform = "rotate(0deg)";
+  }
+}
+
+function Answer4() {
+  if (document.getElementById("answer4").style.display === "none") {
+    document.getElementById("answer4").style.display = "block";
+    document.getElementById("svg4").style.transform = "rotate(180deg)";
+  } else {
+    document.getElementById("answer4").style.display = "none";
+    document.getElementById("svg4").style.transform = "rotate(0deg)";
+  }
+}
+
+function Answer5() {
+  if (document.getElementById("answer5").style.display === "none") {
+    document.getElementById("answer5").style.display = "block";
+    document.getElementById("svg5").style.transform = "rotate(180deg)";
+  } else {
+    document.getElementById("answer5").style.display = "none";
+    document.getElementById("svg5").style.transform = "rotate(0deg)";
+  }
+}
