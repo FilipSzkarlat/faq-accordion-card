1ea5dd52fa6b535ac67b37a59ac64963b2366f85
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-01-02 22:43:37 +0100

    add bold title to onClick function

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ function Answer1() {
   if (document.getElementById("answer1").style.display === "none") {
     document.getElementById("answer1").style.display = "block";
     document.getElementById("svg1").style.transform = "rotate(180deg)";
+    document.getElementById("question1").style.fontWeight = "700";
   } else {
     document.getElementById("answer1").style.display = "none";
     document.getElementById("svg1").style.transform = "rotate(0deg)";
+    document.getElementById("question1").style.fontWeight = "400";
   }
 }
 
@@ -12,9 +14,11 @@ function Answer2() {
   if (document.getElementById("answer2").style.display === "none") {
     document.getElementById("answer2").style.display = "block";
     document.getElementById("svg2").style.transform = "rotate(180deg)";
+    document.getElementById("question2").style.fontWeight = "700";
   } else {
     document.getElementById("answer2").style.display = "none";
     document.getElementById("svg2").style.transform = "rotate(0deg)";
+    document.getElementById("question2").style.fontWeight = "400";
   }
 }
 
@@ -22,9 +26,11 @@ function Answer3() {
   if (document.getElementById("answer3").style.display === "none") {
     document.getElementById("answer3").style.display = "block";
     document.getElementById("svg3").style.transform = "rotate(180deg)";
+    document.getElementById("question3").style.fontWeight = "700";
   } else {
     document.getElementById("answer3").style.display = "none";
     document.getElementById("svg3").style.transform = "rotate(0deg)";
+    document.getElementById("question3").style.fontWeight = "400";
   }
 }
 
@@ -32,9 +38,11 @@ function Answer4() {
   if (document.getElementById("answer4").style.display === "none") {
     document.getElementById("answer4").style.display = "block";
     document.getElementById("svg4").style.transform = "rotate(180deg)";
+    document.getElementById("question4").style.fontWeight = "700";
   } else {
     document.getElementById("answer4").style.display = "none";
     document.getElementById("svg4").style.transform = "rotate(0deg)";
+    document.getElementById("question4").style.fontWeight = "400";
   }
 }
 
@@ -42,8 +50,10 @@ function Answer5() {
   if (document.getElementById("answer5").style.display === "none") {
     document.getElementById("answer5").style.display = "block";
     document.getElementById("svg5").style.transform = "rotate(180deg)";
+    document.getElementById("question5").style.fontWeight = "700";
   } else {
     document.getElementById("answer5").style.display = "none";
     document.getElementById("svg5").style.transform = "rotate(0deg)";
+    document.getElementById("question5").style.fontWeight = "400";
   }
 }
